1e7d843aeed8ac41093d818bc37e7438e82a557f
ursgraf <urs.graf@ntb.ch>
2020-03-10 12:38:52 +0100

    Change to reference

--- sequence/step/WaitForLevel.hpp
+++ sequence/step/WaitForLevel.hpp
@@ -13,11 +13,9 @@ namespace eeduro{
 		class WaitForLevel : public Step {
 			public:
 				WaitForLevel(std::string name, Sequence* caller, SafetySystem& safetySys) : Step(name, caller), safetySys(safetySys) { }
-				int operator() (SafetyLevel level) {this->level = &level; return start();}
+				int operator() (SafetyLevel& level) {this->level = &level; return start();}
 				int action() { };
-				bool checkExitCondition() {
-					return safetySys.getCurrentLevel() == *level;
-				}
+				bool checkExitCondition() {return safetySys.getCurrentLevel() == *level;}
 			
 			private:
 				SafetySystem& safetySys;
